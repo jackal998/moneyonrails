83d52f539f9227727c0bd7901e7eba28d07ca711
jackal998 <ej_lin2000@yahoo.com.tw>
2021-07-23 13:55:09 +0800

    Basic frame of index page (CSS/JS) work ok, functional JS not loaded yet (charts not ready)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,16 +2,20 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "bootstrap/dist/js/bootstrap"
-import "bootstrap/dist/css/bootstrap"
+import "aos"
+import "./src/application.scss"
+import "bootstrap"
 
-import './src/application.scss'
+import('./js/settings')
+import('./js/custom.min')
 
 Rails.start()
 Turbolinks.start()
