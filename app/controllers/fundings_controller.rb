2d2f59f012f2c8d8ae83673e5e3728a425ded596
jackal998 <ej_lin2000@yahoo.com.tw>
2021-07-28 15:46:02 +0800

    index Table done

--- app/controllers/fundings_controller.rb
+++ app/controllers/fundings_controller.rb
@@ -1,8 +1,7 @@
 class FundingsController < ApplicationController
   def index
 
+    helpers.update_market_infos if Time.now - CurrentFundStat.last.updated_at > 15
     @coins = Coin.includes(:current_fund_stat).where(current_fund_stat: {market_type: "normal"}).order("current_fund_stat.irr_past_month desc")
   end
-
-  
 end
