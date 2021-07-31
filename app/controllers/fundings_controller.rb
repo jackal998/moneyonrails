39480b60eda60e9ba941e8b1674ac8ecf7ae0009
jackal998 <ej_lin2000@yahoo.com.tw>
2021-08-01 02:18:41 +0800

    index with basic account infos

--- app/controllers/fundings_controller.rb
+++ app/controllers/fundings_controller.rb
@@ -1,7 +1,20 @@
 class FundingsController < ApplicationController
+  require 'ftx_client'
   def index
+    balances = {"totalusdValue" => 0.00}
 
     helpers.update_market_infos if Time.now - CurrentFundStat.last.updated_at > 15
     @coins = Coin.includes(:current_fund_stat).where(current_fund_stat: {market_type: "normal"}).order("current_fund_stat.irr_past_month desc")
+    
+    @ftx_account = FtxClient.account
+
+    @ftx_wallet_balances = FtxClient.wallet_balances
+    if @ftx_wallet_balances["success"] 
+      @ftx_wallet_balances["result"].each do |result|
+        balances[result["coin"]] = result["usdValue"]
+        balances["totalusdValue"] += result["usdValue"]
+      end
+    end
+    render locals: {balances: balances}
   end
 end
