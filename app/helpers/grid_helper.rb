151dd17d4388625da12da2939133e4b6d4b4111d
jackal998 <ej_lin2000@yahoo.com.tw>
2021-09-04 23:19:25 +0800

    grid index js calc ready for spot as input

--- app/helpers/grid_helper.rb
+++ app/helpers/grid_helper.rb
@@ -1,2 +1,10 @@
 module GridHelper
+  def decimals(a)
+    num = 0
+    while(a != a.to_i)
+        num += 1
+        a *= 10
+    end
+    num   
+  end
 end
--- app/javascript/packs/js/grid.js
+++ app/javascript/packs/js/grid.js
@@ -7,15 +7,15 @@ function gridreadyFn( jQuery ) {
     grid_setup(grid_setting)
 
     function grid_setup(grid_setting) {
-        if (!grid_setting) {
-            return;
-        }
+        if (!grid_setting) {return};
         var lower_limit = document.getElementById("lower_limit");
         var upper_limit = document.getElementById("upper_limit");
         var grids = document.getElementById("grids");
         var grid_gap = document.getElementById("grid_setting_grid_gap");
         var input_USD_amount = document.getElementById("input_USD_amount");
         var input_spot_amount = document.getElementById("input_spot_amount");
+        var input_USD_detail = document.getElementById("input_USD_detail");
+        var input_spot_detail = document.getElementById("input_spot_detail");
         var market_price = document.getElementById("market_price");
         var pricestep = document.getElementById("pricestep");
         var threshold = document.getElementById("threshold");
@@ -41,7 +41,9 @@ function gridreadyFn( jQuery ) {
           var gap_value = new Decimal(grid_gap.value);
 
           var market_value = new Decimal(market_price.value);
-          var usd_value = new Decimal(input_USD_amount.value);
+          var usd_in_value = new Decimal(input_USD_amount.value);
+          var spot_in_value = new Decimal(input_spot_amount.value);
+          var spot_step = new Decimal(input_spot_amount.step);
           var threshold_value = new Decimal(threshold.value);
 
           function validate(name) {
@@ -80,12 +82,13 @@ function gridreadyFn( jQuery ) {
           function configcalc(name) {
             // calc upper_value & gap_value
             var max_grids = upper_value.sub(lower_value).div(price_step).add('1');
-            if (grids_value.gt(max_grids)) {
-              grids_value = max_grids;
-            };
+            if (grids_value.gt(max_grids)) {grids_value = max_grids};
 
             gap_value = upper_value.sub(lower_value).div(price_step).divToInt(grids_value.sub('1')).mul(price_step);
             upper_value = lower_value.add(grids_value.sub('1').mul(gap_value))
+            grid_gap.value = gap_value.toFixed();
+            upper_limit.value = upper_value.toFixed();
+            grids.value = grids_value.toFixed();
             // end calc upper_value & gap_value
             
             // calc sell/buy grids
@@ -109,33 +112,66 @@ function gridreadyFn( jQuery ) {
             }
             // end calc sell/buy grids
 
-            // calc USD required
-            sell_grids_USD_required = sell_grids.mul(market_on_grid_value);
-            buy_grids_USD_required = buy_grids.mul(buy_grids.sub('1')).div('2').mul(gap_value).add(buy_grids.mul(lower_value));
+            var setting_spot_required = new Decimal('0');
+            var setting_usd_required = new Decimal('0');
+            var spot_required = new Decimal('0');
+            var usd_required = new Decimal('0');
+            var spot_inuse = new Decimal('0');
 
-            var usd_step = sell_grids_USD_required.add(buy_grids_USD_required).mul(size_step);
-            // end calc USD required
-
-            grids.value = grids_value.toFixed();
-            grid_gap.value = gap_value.toFixed();
-            upper_limit.value = upper_value.toFixed();
-            input_USD_amount.step = usd_step.toFixed(2);
+            function usdcalc(size_value) {
+              setting_spot_required = sell_grids.mul(size_value);
+              setting_usd_required = buy_grids.mul(buy_grids.sub('1')).div('2').mul(gap_value).add(buy_grids.mul(lower_value)).mul(size_value);
+// console.log(`sell_grids : ${sell_grids.toFixed(2)}`);
+// console.log(`buy_grids : ${buy_grids.toFixed(2)}`);
+// console.log(`setting_spot_required : ${setting_spot_required.toFixed(2)}`);
+// console.log(`setting_usd_required : ${setting_usd_required.toFixed(2)}`);
+// console.log(`size_value : ${size_value.toFixed(2)}`);
+              if (spot_in_value.lt(setting_spot_required)) {
+                spot_required = setting_spot_required.sub(spot_in_value)
+                spot_inuse = spot_in_value
+              } else {
+                spot_required = new Decimal('0');
+                spot_inuse = setting_spot_required
+              }
+// console.log(`spot_required : ${spot_required.toFixed(2)}`);
+              usd_required = setting_usd_required.add(spot_required.mul(market_on_grid_value));
+              console.log(`usdcalc: usd_required : ${usd_required.toFixed(2)}`);              
+              return usd_required
+            }
+            
+            usd_required = usdcalc(size_value)
 
             if (name == 'input_USD_amount') {
-              size_value = usd_value.div(usd_step).mul(size_step)
+// console.log(usd_in_value.toFixed(2));
+              if (usd_in_value.gt(usd_required)) {
+                size_value = size_value.add(size_step);
+              } else if (usd_in_value.lt(usd_required) && size_value.gt(size_step)) {
+                size_value = size_value.sub(size_step);
+              }
               order_size.value = size_value.toNearest(size_step, Decimal.ROUND_HALF_UP);
-            } else {
-              usd_value = usd_step.mul(size_value.div(size_step));
-              input_USD_amount.value = usd_value.toFixed(2);
+              usd_required = usdcalc(size_value)
             }
-
-            if (usd_value.gt(input_USD_amount.max) || usd_value.lt(input_USD_amount.min)) {
+            
+            input_USD_amount.value = usd_required.toFixed(2);
+// console.log('===========================');
+            if (usd_required.gt(input_USD_amount.max) || usd_required.lt(input_USD_amount.min)) {
               input_USD_amount.classList.add("is-invalid");
               input_USD_amount.setAttribute('title', '最大金額 ' + input_USD_amount.max + ' USD');
+              input_USD_detail.innerHTML = '最大金額 ' + input_USD_amount.max.toFixed(2) + ' USD';
             } else {
               input_USD_amount.classList.remove("is-invalid");
               input_USD_amount.setAttribute('title', '');
+              input_USD_detail.innerHTML = setting_usd_required.toFixed(2) + ' USD + ' + spot_required.toNearest(spot_step) + ' ' + grid_setting.value;
             }
+
+            if (spot_in_value.gt(input_spot_amount.max) || spot_in_value.lt(input_spot_amount.min)) {
+              input_spot_amount.classList.add("is-invalid");
+              input_spot_amount.setAttribute('title', '最大數量 ' + input_spot_amount.max + ' ' + grid_setting.value);
+            } else {
+              input_spot_amount.classList.remove("is-invalid");
+              input_spot_amount.setAttribute('title', '');
+            }
+            input_spot_detail.innerHTML = '使用 ' + spot_inuse.toNearest(spot_step) + ' ' + grid_setting.value;
           };
           validate(name);
           configcalc(name);
