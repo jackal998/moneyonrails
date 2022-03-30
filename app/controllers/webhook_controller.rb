07ef95c7af2b262ec6071a1b1786fb871f440543
jackal998 <ej_lin2000@yahoo.com.tw>
2022-03-30 15:49:40 +0800

    webhook ready

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,11 +2,22 @@ class WebhookController < ApplicationController
   protect_from_forgery except: :receiver
 
   def receiver
-  #   message = JSON.parse(request.body.read)
+    sub_account = tv_params["strategy_name"]
+    market = tv_params["order_market"].split("USDT")[0] + "-PERP"
+    side = tv_params["order_action"]
+    size = tv_params["order_size"]
 
-  #   puts message
-    puts params["name"]
+    payload = {market: market, side: side, price: nil, type: "market", size: size}
+    
+    puts payload
+    order_result = FtxClient.place_order(sub_account, payload) unless size == 0
+    puts order_result
 
-    render plain: 'Response Saved', status: 200
+    render plain: 'ok', status: 200
+  end
+
+private
+  def tv_params
+    params.permit(:strategy_name, :order_action, :order_size, :order_price, :order_market, :strategy_position_size )
   end
 end
