ee2ff0de4fe15e082f73a38fac54a719a1d3503f
E.J <ej_lin2000@yahoo.com.tw>
2022-06-25 18:01:57 +0800

    move market update_infos to rake

--- app/controllers/fundings_controller.rb
+++ app/controllers/fundings_controller.rb
@@ -1,7 +1,6 @@
 class FundingsController < ApplicationController
   require 'ftx_client'
   def index
-    helpers.update_market_infos
     @coins = Coin.includes(:current_fund_stat).where(current_fund_stat: {market_type: "normal"}).order("current_fund_stat.irr_past_month desc")
     
     coin_name = params["coin_name"] ? params["coin_name"] : "BTC"
